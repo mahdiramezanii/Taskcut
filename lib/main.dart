a287ade703165f76b057a487fb12363f0cd12927
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-03-18 18:54:57 +0330

    initial Hive in Taskut

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 import 'package:taskcut/screan/home_screan.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("names");
   runApp(Application());
 }
 
