d416c3f4b6c66f96c30e57452ec30d7a8af25e31
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-03-24 21:19:55 +0330

    complate datetime in task-> add date wehn initial task and edit

--- lib/edit_task_screan.dart
+++ lib/edit_task_screan.dart
@@ -2,6 +2,7 @@ import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:hive_flutter/hive_flutter.dart";
 import "package:taskut_application/task.dart";
+import "package:time_pickerr/time_pickerr.dart";
 
 class EditTaskScrean extends StatefulWidget {
   @override
@@ -15,6 +16,7 @@ class _EditTaskScreanState extends State<EditTaskScrean> {
   var tsakBox = Hive.box<Task>("task");
   FocusNode negahban1 = FocusNode();
   FocusNode negahban2 = FocusNode();
+  DateTime? _time;
 
   TextEditingController? textController;
   TextEditingController? subTextController;
@@ -37,89 +39,108 @@ class _EditTaskScreanState extends State<EditTaskScrean> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 200,
-          ),
-          Directionality(
-            textDirection: TextDirection.rtl,
-            child: TextField(
-              controller: textController,
-              maxLines: 1,
-              focusNode: negahban1,
-              decoration: InputDecoration(
-                hintText: "عنوان تسک را وارد کنید",
-                label: Text("عنوان را وارد کنید"),
-                labelStyle: TextStyle(
-                    color: Colors.green,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(width: 2, color: Colors.red),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(width: 4, color: Colors.green),
-                ),
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Directionality(
-            textDirection: TextDirection.rtl,
-            child: TextField(
-              controller: subTextController,
-              maxLines: 2,
-              focusNode: negahban2,
-              decoration: InputDecoration(
-                hintText: "توضیحات تسک را وارد کنید",
-                label: Text("توضیحات تسک"),
-                labelStyle: TextStyle(
-                    color: Colors.green,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(width: 2, color: Colors.red),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(width: 4, color: Colors.green),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: TextField(
+                controller: textController,
+                maxLines: 1,
+                focusNode: negahban1,
+                decoration: InputDecoration(
+                  hintText: "عنوان تسک را وارد کنید",
+                  label: Text("عنوان را وارد کنید"),
+                  labelStyle: TextStyle(
+                      color: Colors.green,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(width: 2, color: Colors.red),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(width: 4, color: Colors.green),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              minimumSize: Size(200, 60),
-              maximumSize: Size(300, 60),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
+            SizedBox(
+              height: 30,
             ),
-            onPressed: () {
-
-              var title=textController!.text;
-              var subtitle=subTextController!.text;
-              widget.task.title=title;
-              widget.task.subTitle=subtitle;
-              widget.task.save();
-
-              Navigator.pop(context);
-            },
-            child: Text(
-              "ذخیره تسک",
-              style: TextStyle(color: Colors.white, fontSize: 20),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: TextField(
+                controller: subTextController,
+                maxLines: 2,
+                focusNode: negahban2,
+                decoration: InputDecoration(
+                  hintText: "توضیحات تسک را وارد کنید",
+                  label: Text("توضیحات تسک"),
+                  labelStyle: TextStyle(
+                      color: Colors.green,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(width: 2, color: Colors.red),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(width: 4, color: Colors.green),
+                  ),
+                ),
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            CustomHourPicker(
+              date: widget.task.time,
+              elevation: 3,
+              onPositivePressed: (context, time) {
+                _time=time;
+                print(time.hour);
+                print(time);
+              },
+              onNegativePressed: (context) {},
+              positiveButtonStyle: TextStyle(color: Colors.blue, fontSize: 20),
+              positiveButtonText: "انتخاب تاریخ",
+              negativeButtonStyle: TextStyle(color: Colors.red, fontSize: 20),
+              negativeButtonText: "لغو",
+              initDate: DateTime.now(),
+              title: "ساعت را وارد کنید",
+              titleStyle: TextStyle(color: Colors.green, fontSize: 24),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                minimumSize: Size(200, 60),
+                maximumSize: Size(300, 60),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+              ),
+              onPressed: () {
+                var title = textController!.text;
+                var subtitle = subTextController!.text;
+                widget.task.title = title;
+                widget.task.subTitle = subtitle;
+                widget.task.time=_time!;
+                widget.task.save();
+
+                Navigator.pop(context);
+              },
+              child: Text(
+                "ذخیره تسک",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
